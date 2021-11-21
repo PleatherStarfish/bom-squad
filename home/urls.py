981ed51effa67265465d1f05e25f6d5e38e764cb
Daniel <lontano.composer@gmail.com>
2021-11-21 17:32:21 -0500

    Add login/logout logic

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,9 @@ from home import views
 from .views import search_results
 
 urlpatterns = [
-    path('search/', views.search_results, name='search_results'),
     path('', views.index, name='home'),
+    path('search/', views.search_results, name='search_results'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register')
 ]
--- home/views.py
+++ home/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import redirect
 from modules.models import Module
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -30,3 +35,40 @@ def search_results(request):
         return render(request, 'home/home.html', context)
     else:
         return redirect(index)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have successfully logged in.'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Login attempt unsuccessful.'))
+            return redirect('login')
+    else:
+        return render(request, 'home/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out.'))
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You have registered...'))
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    context = {'form': form}
+    return render(request, 'home/register.html', context)

5ed57cfedf8d1e263c868822a0b9745914be62e7
Daniel <lontano.composer@gmail.com>
2021-12-22 20:40:02 -0800

    Add working splash page animation

--- modules/views.py
+++ modules/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from modules.models import Module
+from users_extended.models import UserExtended
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def module_detail(request, slug):
     module = Module.objects.filter(slug=slug)
-    context = {"module_list": module}
-    return render(request, 'modules/index.html', context)
+    built = None
+    if request.user.is_authenticated:
+        user = UserExtended.objects.get(user=request.user)
+        built = user.built_modules.all()
+    return render(request, 'modules/index.html', {"module_list": module, "built": built})
+
+@login_required()
+def add_to_built(request, id):
+    if request.method == 'GET':
+        user_id = request.user.id
+        user = UserExtended.objects.get(user__id=user_id)
+        module = Module.objects.get(id=id)
+        user.built_modules.add(module)
+        user.save()
+        return HttpResponseRedirect('/users/djm/')
+
+@login_required()
+def remove_from_built(request, id):
+    if request.method == 'GET':
+        user_id = request.user.id
+        user = UserExtended.objects.get(user__id=user_id)
+        module = Module.objects.get(id=id)
+        user.built_modules.remove(module)
+        user.save()
+        return HttpResponseRedirect('/users/djm/')
--- users_extended/views.py
+++ users_extended/views.py
@@ -4,11 +4,38 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView
+
 
 # Create your views here.
 @login_required()
 def user_page(request, **kwargs):
-    context = {}
-    context['user'] = get_object_or_404(UserExtended, slug=kwargs.get('slug'))
-    return render(request, 'users/index.html', context)
+    built = None
+    if request.user.is_authenticated:
+        user = UserExtended.objects.get(user=request.user)
+        built = user.built_modules.all()
+    user_page = get_object_or_404(UserExtended, slug=kwargs.get('slug'))
+    return render(request, 'users/index.html', {'user': user_page, "built": built})
+
+
+@login_required()
+def add_to_built(request, id):
+    if request.method == 'GET':
+        user_id = request.user.id
+        user = UserExtended.objects.get(user__id=user_id)
+        module = Module.objects.get(id=id)
+        user.built_modules.add(module)
+        user.save()
+        return HttpResponseRedirect('/users/djm/')
+
+
+@login_required()
+def remove_from_built(request, id):
+    if request.method == 'GET':
+        user_id = request.user.id
+        user = UserExtended.objects.get(user__id=user_id)
+        module = Module.objects.get(id=id)
+        user.built_modules.remove(module)
+        user.save()
+        return HttpResponseRedirect('/users/djm/')

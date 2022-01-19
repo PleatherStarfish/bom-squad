22c2b3722663bd7ff5b6931268d9b526d1256089
Daniel <lontano.composer@gmail.com>
2022-01-19 01:52:28 -0500

    Style inventory data in module detail

--- modules/templatetags/custom_tags.py
+++ modules/templatetags/custom_tags.py
@@ -2,10 +2,10 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def get_item(queryset, key):
-    inventory_number = None
     try:
         return queryset.filter(id=key).first().number
     except:
-        pass
+        return 0

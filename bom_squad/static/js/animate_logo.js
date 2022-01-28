be5e7f6badfb4b41811f060e70c156d2aac4fbf0
Daniel <lontano.composer@gmail.com>
2022-01-28 11:57:46 -0500

    Move scripts to js directory

---
+++ bom_squad/static/js/animate_logo.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+
+    // Thanks to StackOverflow user Phil Ricketts https://stackoverflow.com/a/8028584
+    // for this getScrollPercent function.
+    function getScrollPercent() {
+        var h = document.documentElement,
+            b = document.body,
+            st = 'scrollTop',
+            sh = 'scrollHeight';
+        return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+    }
+
+    // function onresize(e) {
+    //    var width = e.target.outerWidth;
+    //    var height = e.target.outerHeight;
+    // }
+    //
+    // window.addEventListener("resize", onresize);
+
+    var offcanvasTop = document.getElementById('offcanvasTop');
+    var logoImg = document.getElementById('logo-img-head-static');
+    var headLogo = document.getElementById('head-logo');
+
+    offcanvasTop.addEventListener('hide.bs.offcanvas', function () {
+        headLogo.classList.remove("hide-logo-img");
+        logoImg.classList.remove("hide-logo-img");
+    });
+    offcanvasTop.addEventListener('show.bs.offcanvas', function () {
+        headLogo.classList.add("hide-logo-img");
+        logoImg.classList.add("hide-logo-img");
+    });
+
+    window.addEventListener("scroll", function(e) {
+        if (getScrollPercent() > 70) {
+            headLogo.classList.add("head-logo-shown");
+            logoImg.classList.add("logo-img-hidden")
+        } else {
+            headLogo.classList.remove("head-logo-shown");
+            logoImg.classList.remove("logo-img-hidden")
+        }
+    });
+
+    lax.init();
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+        return window.scrollY
+    });
+
+    lax.addElements('.logo-img-head-static', {
+        scrollY: {
+            scale: [[0, 'screenHeight/3'], [1, 0.6], {easing: 'easeInOutQuint', inertia: 10}]
+        }
+    });
+    lax.addElements('.head-logo', {
+        scrollY: {
+            scale: [[0, 'screenHeight/3'], [1, 0.6], {easing: 'easeInOutQuint', inertia: 10}]
+        }
+    });
+
+
+    lax.addElements('#main-nav', {
+        scrollY: {
+            height: [[0, 'screenHeight/3'], [90, 70], {cssUnit: "px", easing: 'easeInOutQuint', inertia: 10}]
+        }
+    })
+}

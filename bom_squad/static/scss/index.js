50c69e2d6d28da9dc845e207b021f5077183e20c
Daniel <lontano.composer@gmail.com>
2022-01-29 18:24:27 -0500

    Convert JS script classes to BEM

--- bom_squad/static/scss/index.js
+++ bom_squad/static/scss/index.js
@@ -3,8 +3,9 @@ import 'bootstrap'
 
 window.onload = function () {
 
-    // Thanks to StackOverflow user Phil Ricketts https://stackoverflow.com/a/8028584
+    // Thanks to StackOverflow user Phil Ricketts
     // for this getScrollPercent function.
+    // (https://stackoverflow.com/a/8028584)
     function getScrollPercent() {
         var h = document.documentElement,
             b = document.body,
@@ -13,26 +14,26 @@ window.onload = function () {
         return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
     }
 
-    var offcanvasTop = document.getElementById('offcanvasTop');
+    var fullscreenMenu = document.getElementById('fullscreen-menu__container');
     var mainLogoImg = document.getElementById('logo__main');
-    var headLogo = document.getElementById('head-logo');
+    var secondaryLogo = document.getElementById('logo__secondary');
 
-    offcanvasTop.addEventListener('hide.bs.offcanvas', function () {
-        headLogo.classList.remove("hide-logo-img");
-        mainLogoImg.classList.remove("hide-logo-img");
+    fullscreenMenu.addEventListener('hide.bs.offcanvas', function () {
+        secondaryLogo.classList.remove("logo__secondary--off-canvas");
+        mainLogoImg.classList.remove("logo__main--off-canvas");
     });
-    offcanvasTop.addEventListener('show.bs.offcanvas', function () {
-        headLogo.classList.add("hide-logo-img");
-        mainLogoImg.classList.add("hide-logo-img");
+    fullscreenMenu.addEventListener('show.bs.offcanvas', function () {
+        secondaryLogo.classList.add("logo__secondary--off-canvas");
+        mainLogoImg.classList.add("logo__main--off-canvas");
     });
 
     window.addEventListener("scroll", function (e) {
         if (getScrollPercent() > 70) {
-            headLogo.classList.add("head-logo-shown");
-            mainLogoImg.classList.add("logo-img-hidden")
+            secondaryLogo.classList.add("logo__secondary--visible");
+            mainLogoImg.classList.add("logo__main--hidden")
         } else {
-            headLogo.classList.remove("head-logo-shown");
-            mainLogoImg.classList.remove("logo-img-hidden")
+            secondaryLogo.classList.remove("logo__secondary--visible");
+            mainLogoImg.classList.remove("logo__main--hidden")
         }
     });
 
@@ -49,13 +50,19 @@ window.onload = function () {
         }
     });
 
-    lax.addElements('.head-logo', {
+    lax.addElements('.logo__secondary', {
         scrollY: {
             scale: [[0, 'screenHeight/3'], [1, 0.6], {easing: 'easeInOutQuint', inertia: 10}]
         }
     });
 
-    lax.addElements('#main-nav', {
+    lax.addElements('.logo__secondary--main-hidden', {
+        scrollY: {
+            scale: [[0, 'screenHeight/3'], [1, 0.7], {easing: 'easeInOutQuint', inertia: 10}]
+        }
+    });
+
+    lax.addElements('#topnav__main', {
         scrollY: {
             height: [[0, 'screenHeight/3'], [90, 70], {cssUnit: "px", easing: 'easeInOutQuint', inertia: 10}]
         }

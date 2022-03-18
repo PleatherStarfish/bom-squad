7978ca3d12a01a178f68069c6004cbf91ac45a1c
Daniel <lontano.composer@gmail.com>
2022-03-18 12:15:10 -0400

    Fix small bug with meta-switches logic in Components React app

--- components/react_app/src/App.js
+++ components/react_app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { Button, Offcanvas, Form } from 'react-bootstrap';
+import Row from 'components/row'
 
 
 function getTotalPrice(number, price) {
@@ -38,9 +39,8 @@ function App() {
     const [allCSwitchesOn, setAllCSwitchesOn] = useState(false);
     const [allSSwitchesOn, setAllSSwitchesOn] = useState(false);
 
-    useEffect(() => console.log(componentsChecked), [componentsChecked]);
 
-    //
+    // Handle a click on the main button that expands the offcanvas div
     const handleOffcanvasButtonClick = () => {
         const username = window.username;
         setComponentsData(JSON.parse(localStorage.getItem(`${username}_comp_data`)));
@@ -57,6 +57,7 @@ function App() {
         setShow(true)
     };
 
+    // Handle a click on any of the switches for the data rows
     const handleSwitchesChange = (e, type) => {
         const switchID = e.target.id.split('_')[1];
         const username = window.username;
@@ -72,11 +73,13 @@ function App() {
 
         if (type === 'components') {
             switchHandler(componentsChecked, switchID, setComponentsChecked)
-        } else {
+        }
+        if (type === 'shopping') {
             switchHandler(shoppingChecked, switchID, setShoppingChecked)
         }
     };
 
+    // Handle any click on the "meta" switches at the top of the switch columns
     const handleMetaSwitchChange = (e, type) => {
         if (type === 'components') {
             setAllCSwitchesOn(!allCSwitchesOn)
@@ -85,6 +88,7 @@ function App() {
         }
     };
 
+    // If the "allCSwitchesOn" state is true, switch all switches to the "on" state, else "off"
     useEffect(() => {
         if (componentsData) {
             if (allCSwitchesOn) {
@@ -92,55 +96,26 @@ function App() {
             } else {
                 setComponentsChecked(new Set([]))
             }
+        }
+    }, [allCSwitchesOn]);
 
+    // If the "allSSwitchesOn" state is true, switch all switches to the "on" state, else "off"
+    useEffect(() => {
+        if (componentsData) {
             if (allSSwitchesOn) {
                 setShoppingChecked(new Set([...Object.keys(componentsData)]))
             } else {
                 setShoppingChecked(new Set([]))
             }
         }
-    }, [allCSwitchesOn, allSSwitchesOn]);
+    }, [allSSwitchesOn]);
 
     useEffect(() => {
         let rows = null;
         if (componentsData) {
             rows = Object.keys(componentsData).map((value, index) => {
                 return (
-                    <tr>
-                        <td>{componentsData[value].description}</td>
-                        <td>{componentsData[value].supplier_short_name}</td>
-                        <td>{componentsData[value].item_no}</td>
-                        <td>{componentsData[value].quantity}</td>
-                        <td style={{visibility: "hidden"}}></td>
-                        <td style={{fontSize: "16px"}}>
-                            <Form>
-                                <Form.Check
-                                    type="switch"
-                                    id={`contentSwitch_${value}`}
-                                    checked={componentsChecked.has(value)}
-                                    onChange={(e) => handleSwitchesChange(e, 'components')}
-                                />
-                            </Form>
-                        </td>
-                        <td style={{fontSize: "16px"}}>
-                            <Form>
-                                <Form.Check
-                                    type="switch"
-                                    id={`shoppingSwitch_${value}`}
-                                    checked={shoppingChecked.has(value)}
-                                    onChange={(e) => handleSwitchesChange(e, 'shopping')}
-                                />
-                            </Form>
-                        </td>
-                        <td><Button className="components__offcanvas-remove-btn" variant="outline-danger" size="sm">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                     className="bi bi-trash3" viewBox="0 0 16 16">
-                                    <path
-                                        d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-                                </svg>
-                            </Button>
-                        </td>
-                    </tr>
+                    <Row componentsData={componentsData} valueString={value} componentsChecked={componentsChecked} shoppingChecked={shoppingChecked} handleSwitchesChange={handleSwitchesChange} />
                 )
             });
             setTableRows(rows)
